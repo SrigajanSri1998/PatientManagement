512bd6922023e0498a75cf3c522ade4945f17bac
srigajan <davidkajan1998@gmail.com>
2020-04-13 10:09:45 +0530

    create the patientService restapi

---
+++ src/com/PatientService.java
@@ -0,0 +1,28 @@
+package com;
+
+import model.Patient;
+
+//For REST Service 
+import javax.ws.rs.*; 
+import javax.ws.rs.core.MediaType; 
+
+//For JSON 
+import com.google.gson.*; 
+
+//For XML 
+import org.jsoup.*; 
+import org.jsoup.parser.*; 
+import org.jsoup.nodes.Document; 
+
+@Path("/Items") 
+public class PatientService {
+	Patient patientObj = new Patient(); 
+	 
+	 @GET  
+	 @Path("/")  
+	 @Produces(MediaType.TEXT_HTML)  
+	 public String readItems()  {   
+		 return "Hello";  
+	}
+	
+}
